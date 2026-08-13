5b34c7a85ab1935854e7fb22f5bb478e2a4bdac1
ciobanuceba <ciobanuceban@gmail.com>
2026-08-13 08:32:40 +0600

    Update index.js

--- artifacts/api-server/api/index.js
+++ artifacts/api-server/api/index.js
@@ -1,15 +1,4 @@
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  return res.json({ 
-    status: 'ok', 
-    message: 'Moldova Visa API Running',
-    path: req.url 
-  });
+  return res.json({ status: 'ok', message: 'API Running' });
 }
